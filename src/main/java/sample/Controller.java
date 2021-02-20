4c3ab82c30ade3a95f6f5b1169ab82c089498c81
Berger <59346355+Berger777@users.noreply.github.com>
2021-02-20 15:58:00 +0100

    Database Connectivity and Tables

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Controller {
+
+    public void irgendwie(ActionEvent event){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:kino.sqlite");
+            Statement stat = conn.createStatement();
+            String sql = "select * from Nutzer;";
+            ResultSet rs = stat.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println("ArtikelNr: " + rs.getString("ArtikelNr") +
+                        ", Bezeichnung: " + rs.getString("Artikelname"));
+            }
+
+            rs.close();
+            conn.close();
+        }
+        catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,16 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.Enumeration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Kinoverwaltungsoftware 1.0");
+        primaryStage.setScene(new Scene(root, 1000, 777));
         primaryStage.show();
     }
 
