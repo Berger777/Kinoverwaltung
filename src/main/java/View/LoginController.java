41e174752ab63351b2deb8ab72544f4b7b367e9d
Matthias <matze7x@googlemail.com>
2021-03-08 13:31:49 +0100

    GUI fxml

--- src/main/java/View/LoginController.java
+++ src/main/java/View/LoginController.java
@@ -4,12 +4,21 @@ import Entities.Nutzer;
 import Exceptions.NutzerNotFoundException;
 import Services.DatabaseService;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class Controller {
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginController {
 
     public TextField vornameTextfield;
     public TextField nachnameTextfield;
@@ -23,7 +32,6 @@ public class Controller {
             Nutzer nutzer = databaseService.getNutzerAusDB(vornameTextfield.getText(),nachnameTextfield.getText());
             if(passwortField.getText().equals(nutzer.getPasswort())){
                 loginPane.setVisible(false);
-                kinoPane.setVisible(true);
             }else{
                 passwortField.setText("Passwort falsch!");
             }
@@ -31,5 +39,20 @@ public class Controller {
             vornameTextfield.setText("Nutzer nicht");
             nachnameTextfield.setText("gefunden!");
         }
+        try {
+            Parent secondStage = FXMLLoader.load(getClass().getResource("/gui.fxml"));
+            Scene scene = new Scene(secondStage, 1000, 777);
+            Stage stage = new Stage();
+            stage.setTitle("Startseite");
+            stage.setScene(scene);
+            FXMLLoader.load(getClass().getResource("/gui.fxml"));
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+            stage.show();
+        } catch (IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
     }
+
+
 }
