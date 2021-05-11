471dd96fd107182629a5e09cc0656a67c5c5035c
Matthias <matze7x@googlemail.com>
2021-05-11 20:28:37 +0200

    Uebersicht

--- src/main/java/View/UebersichtController.java
+++ src/main/java/View/UebersichtController.java
@@ -1,4 +1,18 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class UebersichtController {
+    public Button buchungsseiteButton;
+    public Button logoutButton;
+
+
+    public void buchungseite(ActionEvent actionEvent) {
+
+    }
+
+    public void logout(ActionEvent actionEvent) {
+
+    }
 }
